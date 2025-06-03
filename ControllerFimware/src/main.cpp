83c80a3d4a152ad8382215f55d0c30a59116c64e
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2025-06-03 12:57:23 -0400

    reorganize project

--- ControllerFimware/src/main.cpp
+++ ControllerFimware/src/main.cpp
@@ -4,8 +4,8 @@
 #include <GxEPD2_4C.h>
 #include <GxEPD2_7C.h>
 #include <GxEPD2_BW.h>
-#include <WiFiMulti.h>
 #include <HTTPClient.h>
+#include <WiFiMulti.h>
 
 // select the display class and display driver class in the following file (new
 // style):
@@ -25,38 +25,34 @@
 // for handling alternative SPI pins (ESP32, RP2040) see example
 // GxEPD2_Example.ino
 
-// void sayText();
+// void displayText();
 
 WiFiMulti wifiMulti;
+String content;
 
 void setup() {
 
+  content = "";
   Serial.begin(115200);
 
   Serial.println();
   Serial.println();
   Serial.println();
 
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
+  // for (uint8_t t = 4; t > 0; t--) {
+  //   Serial.printf("[SETUP] WAIT %d...\n", t);
+  //   Serial.flush();
+  //   delay(1000);
+  // }
 
   wifiMulti.addAP("SSID", "PASSWD");
-
-  // display.init(115200); // default 10ms reset pulse, e.g. for bare panels
-  // with DESPI-C02
-  display.init(115200, true, 2,
-               false); // USE THIS for Waveshare boards with "clever" reset
-                       // circuit, 2ms reset pulse
-  // sayText();
-  // display.hibernate();
+  display.init(115200, true, 2, false);
 }
 
-
-void sayText(String dispText) {
-  display.setRotation(2);
+void displayText(String dispText) {
+  Serial.printf("display:\n%s\n", dispText);
+  content = dispText;
+  display.setRotation(0);
   display.setFont(&FreeMonoBold9pt7b);
   display.setTextColor(GxEPD_BLACK);
   int16_t tbx, tby;
@@ -82,39 +78,29 @@ void loop() {
     HTTPClient http;
 
     Serial.print("[HTTP] begin...\n");
-    // configure traged server and url
-    // http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
-    http.begin("http://10.0.0.126:7272"); // HTTP
+    http.begin("http://10.0.0.126:7272");
+    int status = http.GET();
+    if (status > 0) {
+      Serial.printf("[HTTP] GET... code: %d\n", status);
 
-    Serial.print("[HTTP] GET...\n");
-    // start connection and send HTTP header
-    int httpCode = http.GET();
-
-    // httpCode will be negative on error
-    if (httpCode > 0) {
-      // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-
-      // file found at server
-      if (httpCode == HTTP_CODE_OK) {
+      if (status == HTTP_CODE_OK) {
         String payload = http.getString();
         Serial.println(payload);
-        sayText(payload);
+        if (payload != content){
+          displayText(payload);
+        }
       }
     } else {
       Serial.printf("[HTTP] GET... failed, error: %s\n",
-                        http.errorToString(httpCode).c_str());
+                    http.errorToString(status).c_str());
     }
 
     http.end();
-    if (httpCode == HTTP_CODE_OK) {
-      delay(60000);
-    }else{
-      delay(10000);
-    }
-  }else{
+
+    delay(10000);
+
+  } else {
 
     delay(2000);
   }
-
 };
