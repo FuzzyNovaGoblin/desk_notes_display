ad6346a7343486d91a9047aebd6735afb09a4d6d
FuzzyNovaGoblin <fuzzy.nova.goblin@outlook.com>
2026-05-12 19:23:45 -0400

    add more font options and remove hard coded values based on fonts

--- ControllerFimware/src/main.cpp
+++ ControllerFimware/src/main.cpp
@@ -1,24 +1,32 @@
 #include <Arduino.h>
-#include <Fonts/FreeMonoBold12pt7b.h>
 #include <GxEPD2_3C.h>
 #include <GxEPD2_4C.h>
 #include <GxEPD2_7C.h>
 #include <GxEPD2_BW.h>
 #include <HTTPClient.h>
 #include <WiFiMulti.h>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 #include "GxEPD2_display_selection_new_style.h"
+#include "JetBrains_Mono_Bold_Nerd_Font_Complete_Mono15pt7b.h"
 #include "config.h"
 
 void displayText(String);
 
 WiFiMulti wifiMulti;
-String content;
+String last_displayed;
 const String serverUri = "http://" SERVER_IP ":" SERVER_PORT;
+
+const GFXfont &FONT = JetBrains_Mono_Bold_Nerd_Font_Complete_Mono15pt7b;
+const int DISP_W = 800;
+const int DISP_H = 480;
+const int MAX_STRING_WIDTH = ceil(((float)DISP_W) / FONT.glyph[0].xAdvance);
 
 void setup() {
 
-  content = "";
+  last_displayed = "";
   Serial.begin(115200);
 
   Serial.println();
@@ -29,7 +37,6 @@ void setup() {
   display.init(115200, true, 2, false);
   delay(500);
 }
-
 
 void loop() {
   // wait for WiFi connection
@@ -45,9 +52,35 @@ void loop() {
 
       if (status == HTTP_CODE_OK) {
         String payload = http.getString();
-        Serial.println(payload);
-        if (payload != content) {
-          displayText(payload);
+        if (payload != last_displayed) {
+          last_displayed = payload;
+
+          String rebuild_string = "";
+
+          int current_width = 0;
+          for (int i = 0; i < payload.length(); i++) {
+            current_width += 1;
+            // Serial.printf("cur:%i\n", current_width);
+            switch (payload[i]) {
+            case '\n':
+              rebuild_string += '\n';
+              current_width = 0;
+              break;
+
+            default:
+              if (current_width <= MAX_STRING_WIDTH) {
+                rebuild_string += payload[i];
+                // Serial.printf("added char\n");
+              }else{
+                Serial.printf("drop char\n");
+              }
+              break;
+            }
+            Serial.flush();
+          }
+
+          Serial.println(rebuild_string);
+          displayText(rebuild_string);
         }
       }
     } else {
@@ -66,16 +99,17 @@ void loop() {
 };
 
 void displayText(String dispText) {
+  Serial.printf("MAX_STRING_WIDTH: %i\n", MAX_STRING_WIDTH);
+
   Serial.printf("display:\n%s\n", dispText);
-  content = dispText;
   display.setRotation(2);
-  display.setFont(&FreeMonoBold12pt7b);
+  display.setFont(&FONT);
   display.setTextColor(GxEPD_BLACK);
   int16_t tbx, tby;
   uint16_t tbw, tbh;
   display.getTextBounds(dispText, 0, 0, &tbx, &tby, &tbw, &tbh);
   uint16_t x = 0;
-  uint16_t y = 15;
+  uint16_t y = FONT.yAdvance * 0.75;
   display.setFullWindow();
   display.firstPage();
   do {
--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -6,7 +6,7 @@ use crate::config::*;
 
 mod config;
 
-const SCREEN_CHAR_WIDTH:usize = 61;
+// const SCREEN_CHAR_WIDTH:usize = 61;
 
 static TAB_REGEX: OnceLock<Regex> = OnceLock::<Regex>::new();
 static TASK_REGEX: OnceLock<Regex> = OnceLock::<Regex>::new();
@@ -49,13 +49,13 @@ async fn handler() -> String {
         .lines()
         .filter(|l| !l.trim().is_empty())
         .map(|l| TAB_REGEX.get().unwrap().replace_all(l, "$1").to_string())
-        .map(|l| {
-            if l.len() <= SCREEN_CHAR_WIDTH {
-                l
-            } else {
-                l.chars().take(SCREEN_CHAR_WIDTH).collect()
-            }
-        })
+        // .map(|l| {
+        //     if l.len() <= SCREEN_CHAR_WIDTH {
+        //         l
+        //     } else {
+        //         l.chars().take(SCREEN_CHAR_WIDTH).collect()
+        //     }
+        // })
         .filter(|l| {
             if under_todo_header == UnderTodoCase::Past {
                 return false;
